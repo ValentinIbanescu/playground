fd6fd93eca99b50cf4cd24fbd4c62557afc562ac
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-09-20 14:20:20 +0300

    args

---
+++ Ruby/calling-methods.rb
@@ -0,0 +1,14 @@
+def times_two(arg1);
+  puts arg1 * 2;
+end
+
+def sum(arg1, arg2);
+  puts arg1 + arg2;
+end
+
+puts times_two 5
+puts times_two(5)
+puts times_two (5)
+puts sum 1, 2
+puts sum(1, 2)
+# puts sum (1, 2)
