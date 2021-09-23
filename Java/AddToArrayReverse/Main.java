a01cd78be313d95873d9fcd01f8474c5c96f83c3
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-09-23 18:23:23 +0300

    added user input cursor

--- Java/AddToArrayReverse/Main.java
+++ Java/AddToArrayReverse/Main.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.print("Enter your data:\n> ");
         Scanner sc = new Scanner(System.in);
         List<String> result = new ArrayList<>();
         while (sc.hasNext()) {
