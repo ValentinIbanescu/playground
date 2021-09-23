9a59f68076cc0b19b45d6e396812836a2eb276fd
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-09-23 07:23:23 +0300

    binary search 2 solutions

---
+++ JS/lc-704-binary-search.js
@@ -0,0 +1,23 @@
+
+ var search = function(nums, target) {
+   // Solution 1 (short): Using IndexOf() function.
+   //  return nums.indexOf(target) > -1 ? nums.indexOf(target) : -1;
+
+   // Solution 2: Binary Search by the book.
+   let left = 0;
+   let right = nums.length - 1;
+   let pivot = 0;
+  //  console.log(pointer);
+   while (left <= right) {
+     pointer = Math.floor(left + (right - left) / 2);
+     if (target < nums[pointer]) {
+       right = pointer - 1;
+      } else if (target > nums[pointer]) {
+       left = pointer + 1;
+      } else return pointer;
+   }
+   return -1;
+};
+
+console.log(search([-1,0,3,5,9,12],9)); // should return 4
+console.log(search([-1,0,3,7,9,12,14],5)); // should return -1
