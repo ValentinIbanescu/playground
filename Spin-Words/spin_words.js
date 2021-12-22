9fe35351ebfe5b7ed87eb7015fc0a3f4021e0f49
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-12-22 15:49:03 +0200

    spin words final solution

--- Spin-Words/spin_words.js
+++ Spin-Words/spin_words.js
@@ -4,12 +4,10 @@
 // In this way we keep all the whitespaces, commas, dots and other non-word characters in their places.
 // 3. Iterate throuh the array of words with map and reverse only those words equal or longer then 5.
 // 4. Join the words and non-word characters of the new array and return the result.
-
 function spin_words(text) {
   let reverseText = text.split(/(\W)/).map(element =>
     (element.length >= 5) ? element.split('').reverse().join('') : element);
     return reverseText.join('');
   }
-
 // For testing purposes change the text in the parantheses below an run the code.
 console.log(spin_words('Change your life, learn to code.'));
--- Spin-Words/spin_words.rb
+++ Spin-Words/spin_words.rb
@@ -4,9 +4,9 @@ def spin_words(string)
   # In this way we keep all the non-word characters in their places.
   array = string.split(/(\W)/)
   # 3. Iterate throuh the array of words and reverse only words w lenght >= 5.
-  reverse_array = array.map {|word| (word.length >= 5) ? word.reverse : word  }
+  reverse_array = array.map { |word| word.length >= 5 ? word.reverse : word }
   # We join the words and non-word characters of the new array and return the result.
-  return reverse_array.join('')
+  reverse_array.join('')
 end
 # For testing purposes change the text in the parantheses below as you wish.
 puts spin_words('Change your life, learn to code.')
