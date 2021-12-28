25e5626fd41e6e7966a3a99612f27b9537a449dc
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-12-28 14:28:28 +0200

    promise working

--- JS/SimpleCounterJS/script.js
+++ JS/SimpleCounterJS/script.js
@@ -9,17 +9,14 @@ button.addEventListener("click", (event) => {
   console.log(event);
   event.preventDefault();
   button.innerText = "Wait";
-  let state = button.disabled;
-  state = true;
-  console.log(state);
+  button.disabled = true;
   count++;
   total = count * 5;
   title.innerText = `${count} cigarette packages`;
   counter.innerText = `${total.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}`;
-  new Promise((resolve) => setTimeout(resolve, 5000))
+  new Promise((resolve) => setTimeout(resolve, 7000))
   .then(() => {
+    button.disabled = false;
     button.innerText = "Next";
-    state = false;
-    console.log(state);
   });
 });
