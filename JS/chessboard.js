53884752be3836a4812189ef7c201bd75490fd42
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-12-28 07:28:28 +0200

    chessboard with variable size

--- JS/chessboard.js
+++ JS/chessboard.js
@@ -1,9 +1,15 @@
+// 1. Beginner method
 let stringChess = "";
-for (let i = 0; i < 8; i += 1) {
-  // ** Generate a string from 32 # and 32 spaces.
-  i % 2 == 0 ? stringChess += " # # # #\n" : stringChess += "# # # # \n";
+size = 8;
+// The first loop is handling the addition of rows vertically
+for (let i = 0; i < size; i += 1) {
+  let row = "";
+// The second loop creates the rows horizontally.
+  for (let i = 1; i < size; i += 1) {
+    i % 2 == 0 ? row += " " : row += "#";
+  }
+  i % 2 == 0 ? stringChess += ` ${row}\n` : stringChess += `${row} \n`;
 }
-// for (let i = 8; i < 64; i += 9) {
-//   stringChess = stringChess.slice(0, i) + "\n" + stringChess.slice(i);
-// }
 console.log(stringChess);
+
+// 2. Advanced method.
