26efb07a83456ee3fbb4593d5421af25aedeb79e
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-12-30 17:31:31 +0200

    simple math combinations calculator

--- Ruby/combinations.rb
+++ Ruby/combinations.rb
@@ -1,14 +1,16 @@
-puts "Combinations of how many? (n)"
+#  Formula of Combinations: nCr = n! / r! * (n - r)!
+puts 'Combinations of how many? (n)'
 num1 = gets.chomp.to_i
 
-puts "Taken how many at a time? (introduce k ≤ n)"
+puts 'Taken how many at a time? (introduce k ≤ n)'
 num2 = gets.chomp.to_i
 
 def factorial(num)
-  (1..num).reduce(1){ |prod, i| prod * i }
+  (1..num).reduce(1) { |prod, i| prod * i }
 end
-def get_combination(num1, num2)
+
+def combination(num1, num2)
   factorial(num1) / (factorial(num2) * factorial(num1 - num2))
 end
 
-puts "Combinations of #{num1} taken #{num2} at a time is #{get_combination(num1, num2)}."
+puts "Combinations of #{num1} taken #{num2} at a time is #{combination(num1, num2)}."
