b5e6144e44cb9ed4035eee2e803f1fe72304fd51
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-12-30 07:30:30 +0200

    size method for linked list

--- JS/linked-list.js
+++ JS/linked-list.js
@@ -28,6 +28,29 @@ class ListNode {
 
 let node1 = new ListNode(6);
 let node2 = new ListNode(2);
+node1.next = node2;
 
-console.log(node1);
-console.log(node2);
+// Linked list class constructor
+class LinkedList {
+  constructor(header = null) {
+    this.header = header;
+  }
+  // Helper method - size;
+  size() {
+    let count = 0;
+    let node = this.header;
+    while (node) {
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
+}
+
+let list1 = new LinkedList(node1);
+let list2 = new LinkedList(node2);
+
+console.log(list1);
+console.log(list1.header.next.data);
+console.log(list1.size());
+console.log(list2.size());
