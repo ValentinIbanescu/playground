ee9847555139a52e6f9c90c2f2cc550dea8aaa64
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-12-31 14:31:31 +0200

    string methods

---
+++ Ruby/basics.rb
@@ -0,0 +1,11 @@
+sum = 3 + 4
+puts sum
+
+first_name = "boris"
+last_name = "beker"
+puts "#{first_name.capitalize} #{last_name.capitalize}"
+
+
+age = 47
+puts "I'm #{age} years old"
+puts "I'm " + age.to_s +  " years old"
