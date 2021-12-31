c024465c5050692586cc356c4f939380685fa38d
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-12-31 15:31:31 +0200

    integers

--- Ruby/basics.rb
+++ Ruby/basics.rb
@@ -1,3 +1,4 @@
+# Assign the value 7 to the variable sum (binding)
 sum = 3 + 4
 puts sum
 
@@ -9,3 +10,16 @@ puts "#{first_name.capitalize} #{last_name.capitalize}"
 age = 47
 puts "I'm #{age} years old"
 puts "I'm " + age.to_s +  " years old"
+
+s = "Programming"
+puts s[0,4]
+puts s[4..-1]
+s[-3..-1] = "able"
+puts s
+
+puts 11 / 4
+puts 11.0 / 4
+
+# Variables
+age = 47
+puts age
