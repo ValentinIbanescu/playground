337ebbd230447d1977d951af9cc920c0055e619b
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2022-03-24 14:11:01 +0200

    test main String... varargs

---
+++ Java/Tests/Tests.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class Tests {
+    public static void main(String... args) {
+//        int portion = 100;
+//        addSeeds(portion); // try to change portion
+//
+//        // now let's print a portion
+//        System.out.println(portion);
+//        // 100, because the method didn't change portion, only its copy
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+
+    /*
+     * The method increases the portion of seeds by 50
+     * and prints the resulting value
+     */
+//    public static void addSeeds(int portion) {
+//        portion += 50;
+//        System.out.println("The increased portion is " + portion);
+//    }
+//    public static int gcd(int a, int b) {
+//        char[] array = new char[-1];
+//        while (b > 0) {
+//            int c = a % b;
+//            a = b;
+//            b = c;
+//        }
+//        return a;
+//    }
+}
